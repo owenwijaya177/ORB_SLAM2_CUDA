0c40459afbc4bf0591ba6f6401344c857d06ab4a
owenwijaya177 <61115686+owenwijaya177@users.noreply.github.com>
2021-07-26 15:19:36 +0800

    Update System.h

--- include/System.h
+++ include/System.h
@@ -22,6 +22,10 @@
 #ifndef SYSTEM_H
 #define SYSTEM_H
 
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
 #include <string>
 #include <thread>
 #include <opencv2/core/core.hpp>
